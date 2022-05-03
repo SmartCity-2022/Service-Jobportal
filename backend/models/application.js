172413d2c8226b4a38ba287111a48b9c6b35a624
cv7l1 <0xcv7l1@protonmail.com>
2022-05-03 17:20:35 +0200

    add application model

---
+++ backend/models/application.js
@@ -0,0 +1,22 @@
+const {DataTypes} = require('sequelize')
+
+module.exports = (sequelize) => {
+  const Application = sequelize.define(
+    'Application',
+    {
+      document: {
+        type: DataTypes.STRING
+      },
+      date: {
+        type: DataTypes.DATEONLY,
+        defaultValue: DataTypes.NOW,
+        allowNull: false
+      },
+      response: {
+        type: DataTypes.BOOLEAN
+      }
+    }
+  )
+
+  return Application
+}

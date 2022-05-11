2da1ed90dfdf5da6c41b5a87db9f811ceb8e9631
cv7l1 <0xcv7l1@protonmail.com>
2022-05-11 13:43:36 +0200

    add citizen routes

--- backend/routes/api/citizens.js
+++ backend/routes/api/citizens.js
@@ -1,8 +1,55 @@
 var router = require('express').Router()
 
 router.get('/:id', function(req, res) {
-  res.status(200)
-  res.send("Hallo")
-});
+  req.app.get('sequelize').models.Citizen.findByPk(req.params.id)
+    .then(function(citizen) {
+      if(!citizen)
+        res.sendStatus(404)
+      else
+        res.json(citizen)
+    })
+})
+
+router.post('/', async function(req, res) {
+  try {
+    let citizen = await req.app.get('sequelize').models.Citizen.create(req.body)
+
+    if(!citizen) 
+      res.sendStatus(401)
+    else
+      res.json(citizen).status(201)
+    
+  } catch(error) {
+    res.sendStatus(401) 
+  }
+})
+
+router.put('/:id', async function(req, res) {
+  try {
+    let citizen = req.app.get('sequelize').models.Citizen.findByPk(req.params.id)
+    citizen = req.app.get('sequelize').models.Citizen.build(citizen)
+
+    citizen.set(req.body)
+    await citizen.save()
+    res.send(citizen).status(200)
+
+  } catch(error) {
+    res.sendStatus(401)
+  }
+})
+
+router.delete('/:id', function(req, res) {
+  try {
+    let citizen = req.app.get('sequelize').models.Citizen.destroy({where: {id: req.params.id}})
+
+    if(!citizen) 
+      res.sendStatus(401)
+    else
+      res.json(citizen).status(201)
+
+  } catch(error) {
+    res.sendStatus(401)
+  }
+})
 
 module.exports = router

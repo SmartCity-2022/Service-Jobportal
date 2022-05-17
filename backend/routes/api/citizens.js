adcfb15d346264e7c2c1d57f3ab72837fdb29aae
cv7l1 <0xcv7l1@protonmail.com>
2022-05-17 21:53:14 +0200

    redo citizens route

--- backend/routes/api/citizens.js
+++ backend/routes/api/citizens.js
@@ -1,55 +1,54 @@
 var router = require('express').Router()
+var auth = require('../auth')
 
-router.get('/:id', function(req, res) {
-  req.app.get('sequelize').models.Citizen.findByPk(req.params.id)
-    .then(function(citizen) {
-      if(!citizen)
-        res.sendStatus(404)
-      else
-        res.json(citizen)
+router.get('/:email', auth.required, async (req, res) => {
+  try {
+    let citizen = await req.app.get('sequelize').models.Citizen.findOne({
+      where: {
+        email: req.params.email
+      }
     })
+    if(!citizen)
+      return res.sendStatus(404)
+    else
+      res.json(citizen).status(200)
+  }
+  catch(error) {
+    res.sendStatus(401)
+  }
 })
 
-router.post('/', async function(req, res) {
+router.post('/', async (req, res) => {
   try {
     let citizen = await req.app.get('sequelize').models.Citizen.create(req.body)
-
-    if(!citizen) 
-      res.sendStatus(401)
-    else
-      res.json(citizen).status(201)
-    
-  } catch(error) {
+    res.json(citizen).status(201)
+  } 
+  catch(error) {
     res.sendStatus(401) 
   }
 })
 
-router.put('/:id', async function(req, res) {
+router.put('/:id', async (req, res) => {
   try {
-    let citizen = req.app.get('sequelize').models.Citizen.findByPk(req.params.id)
-    citizen = req.app.get('sequelize').models.Citizen.build(citizen)
-
-    citizen.set(req.body)
-    await citizen.save()
-    res.send(citizen).status(200)
-
-  } catch(error) {
+    let citizen = await req.app.get('sequelize').models.Citizen.findByPk(req.params.id)
+    citizen = await citizen.update({email: req.body.email}, {where: {email: req.body.email}})
+    return res.json(citizen).status(200)
+  }
+  catch(error) {
     res.sendStatus(401)
   }
 })
 
-router.delete('/:id', function(req, res) {
+router.delete('/:id', async (req, res) => {
   try {
-    let citizen = req.app.get('sequelize').models.Citizen.destroy({where: {id: req.params.id}})
-
-    if(!citizen) 
-      res.sendStatus(401)
-    else
-      res.json(citizen).status(201)
-
-  } catch(error) {
+    let citizen = await req.app.get('sequelize').models.Citizen.destroy({where: {email: req.params.id}})
+    res.sendStatus(200)
+    
+  }
+  catch(error) {
     res.sendStatus(401)
   }
 })
+
 
 module.exports = router

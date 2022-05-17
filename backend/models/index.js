df0cf12a9fe8437c283726adc079400292d98e2a
cv7l1 <0xcv7l1@protonmail.com>
2022-05-17 21:52:00 +0200

    update model relations

--- backend/models/index.js
+++ backend/models/index.js
@@ -5,19 +5,24 @@ module.exports = () => {
   let sequelize
   sequelize = new Sequelize(config.databaseUrl)
 
-  sequelize.sync().then(console.log("Connection etablished"))
-
   const Citizen = require('./citizen')(sequelize)
   const Job = require('./job')(sequelize)
   const Company = require('./company')(sequelize)
   const Application = require('./application')(sequelize)
 
   Citizen.hasMany(Application)
+  Application.belongsTo(Citizen)
+
   Citizen.hasMany(Company)
+  Company.belongsTo(Citizen)
 
   Job.hasMany(Application)
+  Application.belongsTo(Job)
 
   Company.hasMany(Job)
+  Job.belongsTo(Company)
 
+  sequelize.sync().then(console.log("Connection etablished"))
+  
   return sequelize
 }

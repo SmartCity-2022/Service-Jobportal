684a8fa3a1e5ace4df7e467ae51c0ba1341ff3d5
cv7l1 <0xcv7l1@protonmail.com>
2022-05-17 21:55:12 +0200

    add middleware & job routes

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,6 +1,16 @@
 const router = require('express').Router()
 const Sequelize = require('sequelize')
 
-router.use('/', require('./citizens'))
+const assertComp = async(req, res, next) => {
+  const company = await req.app.get('sequelize').models.Company.findOne({where: {id: req.params.companyId}})
+  if(!company)
+    return res.send("company does not exist").status(404)
+  else
+    return next()
+}
+
+router.use('/citizens', require('./citizens'))
+router.use('/companies', require('./company'))
+router.use('/companies/:companyId/jobs', assertComp, require('./jobs'))
 
 module.exports = router

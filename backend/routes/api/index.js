cba49abd0abc0d83ccf657531a1112caed405b42
cv7l1 <0xcv7l1@protonmail.com>
2022-05-17 22:17:56 +0200

    add application router

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -12,5 +12,6 @@ const assertComp = async(req, res, next) => {
 router.use('/citizens', require('./citizens'))
 router.use('/companies', require('./company'))
 router.use('/companies/:companyId/jobs', assertComp, require('./jobs'))
+router.use('/companies/:companyId/jobs/:jobId/applications', assertComp, require('./applications'))
 
 module.exports = router

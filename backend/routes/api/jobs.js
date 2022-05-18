c6065fb0aa55fa557f66ed38deeba5ca6908714c
cv7l1 <0xcv7l1@protonmail.com>
2022-05-18 16:26:15 +0200

    redo job routes to fix wrong mapping

--- backend/routes/api/jobs.js
+++ backend/routes/api/jobs.js
@@ -1,44 +1,80 @@
-//company/id/jobs
-var router = require('express').Router({mergeParams: true})
-var auth = require('../auth')
+const { Op } = require('sequelize')
+const rabbitmq = require('../../rabbitmq')
+const router = require('express').Router()
+const types = require('./types')
+const config = require('../../config')
 
-router.get('/', async(req, res) => {
+router.get('/', async (req, res, next) => {
   try {
     let jobs = await req.app.get('sequelize').models.Job.findAll({
-      attributes: ['name', 'field', 'type', 'worktime'],
-      where: {
-        companyId: req.params.companyId
-      }
+      attributes: ['id', 'name', 'field', 'type', 'worktime']
     })
     res.json(jobs).status(200)
   }
   catch(error) {
-    res.sendStatus(401)
+    next(error)
   }
 })
 
-router.get('/:jobId', async(req, res) => {
+router.get('/results', async (req, res, next) => {
+  if(typeof req.query.name === 'undefined')
+    req.query.name = ''
+  if(typeof req.query.type === 'undefined')
+    req.query.type = '%'
+
   try {
-    let job = await req.app.get('sequelize').models.Job.findByPk(req.params.jobId)
-    res.json(job).status(200)
+    let query = await req.app.get('sequelize').models.Job.findAll({
+      attributes: ['id', 'name', 'field', 'type', 'worktime'],
+      where: {
+        name: {[Op.like]: req.query.name + '%'},
+        type: {[Op.like]: req.query.type}
+      }
+    })
+    res.json(query).status(200)
   }
   catch(error) {
-    res.sendStatus(401)
+    next(error)
   }
 })
 
-router.post('/', async(req, res) => {
-  req.body.CompanyId = req.params.companyId
+router.get('/types', async (req, res, next) => {
+  res.json(types).status(200)
+})
+
+router.get('/:jobId', async (req, res, next) => {
+  try {
+    let job = await req.app.get('sequelize').models.Job.findByPk(req.params.jobId)
+
+    if(!job)
+      res.status(404)
+    else
+      res.json(job).status(200)
+  }
+  catch(error) {
+    next(error)
+  }
+})
+
+router.post('/', async (req, res, next) => {
   try {
     let job = await req.app.get('sequelize').models.Job.create(req.body)
+
+    //Publish new Jobs
+    const connection = await rabbitmq.getConnection()
+    const channel = await connection.createChannel()
+    await channel.assertExchange(config.rabbitmq_exchange, "topic", {durable: false})
+    
+    channel.publish(config.rabbitmq_exchange, "service.jobportal.job_published",
+    Buffer.from(JSON.stringify({id: job.id, name: job.name }))
+    )
     res.json(job).status(201)
   }
-  catch (error) {
-    return res.send(error)
+  catch(error) {
+    next(error)
   }
 })
 
-router.put('/', async(req, res) => {
+router.put('/', async(req, res, next) => {
   req.app.get('sequelize').models.Job.findByPk(req.body.id)
     .then(async(job) => {
       if(!job)
@@ -62,13 +98,13 @@ router.put('/', async(req, res) => {
     })  
 })
 
-router.delete('/', async(req, res) => {
+router.delete('/', async(req, res, next) => {
   try {
-    let job = await req.app.get('sequelize').models.Citizen.destroy({where: {id: req.body.id}})
+    await req.app.get('sequelize').models.Citizen.destroy({where: {id: req.body.id}})
     res.sendStatus(200)
   }
   catch(error) {
-    res.sendStatus(401)
+    next(error)
   }
 })
 

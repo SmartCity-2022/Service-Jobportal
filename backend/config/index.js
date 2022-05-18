c97cdafa60e629797818783d95f4c403fc2db7d9
cv7l1 <0xcv7l1@protonmail.com>
2022-05-18 16:12:29 +0200

    change config
    - remove unnessecary imports
    - rename some variables
    
    + add rabbitmq adress
    + add rabbitmq exchange name

--- backend/config/index.js
+++ backend/config/index.js
@@ -1,9 +1,13 @@
-const path = require('path')
-const fs = require('fs')
 require('dotenv').config({path: __dirname + '/../.env'})
 
 module.exports = {
-  apiPath: process.env.API_PATH || '/api',
-  apiPort: process.env.API_PORT || 3000,
-  databaseUrl: process.env.DATABASE_URL || ''
+  environment: process.env.ENVIRONMENT || 'production',
+
+  api_path: process.env.API_PATH || '/api',
+  api_port: process.env.API_PORT || 3000,
+
+  db_url: process.env.DATABASE_URL || '',
+
+  rabbitmq_url: process.env.RABBITMQ_URL || '',
+  rabbitmq_exchange: process.env.RABBITMQ_EXCHANGE || 'exchange'
 }

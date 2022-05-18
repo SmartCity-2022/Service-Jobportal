29db6ddb9ef6a4888fdd36e2e4211c53fbaf92df
cv7l1 <0xcv7l1@protonmail.com>
2022-05-18 16:05:30 +0200

    add foreign key checks

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,28 +1,24 @@
 const {Sequelize} = require('sequelize')
 const config = require('../config')
 
-module.exports = () => {
-  let sequelize
-  sequelize = new Sequelize(config.databaseUrl)
+let sequelize
+sequelize = new Sequelize(config.db_url)
 
-  const Citizen = require('./citizen')(sequelize)
-  const Job = require('./job')(sequelize)
-  const Company = require('./company')(sequelize)
-  const Application = require('./application')(sequelize)
+const Citizen = require('./citizen')(sequelize)
+const Job = require('./job')(sequelize)
+const Company = require('./company')(sequelize)
+const Application = require('./application')(sequelize)
 
-  Citizen.hasMany(Application)
-  Application.belongsTo(Citizen)
+Citizen.hasMany(Application, { foreignKey: { allowNull: false }, onDelete: 'CASCADE' })
+Application.belongsTo(Citizen) 
 
-  Citizen.hasMany(Company)
-  Company.belongsTo(Citizen)
+Citizen.hasMany(Company, { foreignKey: { allowNull: false }, onDelete: 'CASCADE' })
+Company.belongsTo(Citizen)
 
-  Job.hasMany(Application)
-  Application.belongsTo(Job)
+Job.hasMany(Application, { foreignKey: { allowNull: false }, onDelete: 'CASCADE' })
+Application.belongsTo(Job)
 
-  Company.hasMany(Job)
-  Job.belongsTo(Company)
-
-  sequelize.sync().then(console.log("Connection etablished"))
+Company.hasMany(Job, { foreignKey: { allowNull: false }, onDelete: 'CASCADE' })
+Job.belongsTo(Company)
   
-  return sequelize
-}
+module.exports = sequelize

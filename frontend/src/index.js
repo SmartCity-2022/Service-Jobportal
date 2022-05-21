dc62884ed37712ff7e9aeda520be954a970382c2
cv7l1 <0xcv7l1@protonmail.com>
2022-05-21 21:42:25 +0200

    add index

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Navbar from './components/Navbar';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Navbar/>
+
+    <BrowserRouter>
+      <Route path = "/" element = {<App/>}/>
+    </BrowserRouter>
   </React.StrictMode>
 );

05c03095ffad479748cdfeb3c4424a312c544888
cv7l1 <0xcv7l1@protonmail.com>
2022-05-22 17:29:56 +0200

    add landing page and css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
+      <h3>Microservice Jobportal</h3>
+
+      <p>Dies ist der Microservice Jobportal?</p>
     </div>
   );
 }

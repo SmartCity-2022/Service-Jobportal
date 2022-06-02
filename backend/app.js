db89ec3f5f7f3589e5a22bc2e19de195ebea8f1e
cv7l1 <0xcv7l1@protonmail.com>
2022-06-02 10:32:24 +0200

    add cors credentials parameter

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const app = express()
 app.set('sequelize', models)
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({origin: true, credentials: true}))
 app.use(cookieParser())
 
 app.use(require('./routes'))

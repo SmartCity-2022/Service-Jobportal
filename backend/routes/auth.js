bf7611f8fd4b1457f871b84832154ee47a7abb37
cv7l1 <0xcv7l1@protonmail.com>
2022-06-04 08:41:24 +0200

    authentication will cause the email from the jwt to be added to the database

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,56 +7,30 @@ module.exports.required = async(req, res, next) => {
     return next()
 
   if(!req.cookies)
-    return res.status(401).send("Missing Cookies")
+    return res.status(401)
     
   const accessToken = req.cookies.accessToken
   const refreshToken = req.cookies.refreshToken
 
   if(!accessToken || !refreshToken)
-    return res.status(401).send("Missing Access or Refresh Token")
-  
-  const {payload, expired, error} = parseJwt(accessToken)
+    return res.status(401)
 
-  if(payload && !expired) {
-    console.log("jwt successful for user " + payload)
-
-    req.user = payload
-    return next()
-  }
-
-  if(expired) {
-    await axios.post(config.mainhub_url + "/api/token", {
-      token: refreshToken
-    })
-    .then((res) => {
-      const accessToken = res.body
-
-      if(accessToken)
-        res.cookie("accessToken", accessToken, { domain: ".smartcity.w-mi.de" })
-      
-      const {payload, expired} = parseJwt(accessToken)
-      if(!payload)
-        return res.status(401).send("Token could not be refreshed")
-
-      req.user = payload
-      return next()
-    })
-    .catch((error) => {console.log("Refreshing failed:" + error)}) 
-  }
-
-  return res.status(401).json(error)
-}
-
-function parseJwt(accessToken) {
   try {
     const payload = verify(accessToken, process.env.SECRET)
-      return { payload : payload, expired: false, error: null}
+    
+    let citizen = await req.app.get("sequelize").models.Citizen.findOne({where: {email: payload.email}})
+    if(!citizen) {
+      citizen = await req.app.get("sequelize").models.Citizen.create({email: payload.email})
+    }
+    req.user = citizen
+    return next()
   }
   catch(error) {
-    if(error instanceof TokenExpiredError)
-      return {payload: null, expired: true, error: error}
+    if(error instanceof TokenExpiredError) {
+      res.status(500).json("Token expired")
+    }
     else {
-      return {payload: null, expired: false, error: error}
+      res.status(401).json(error)
     }
   }
 }

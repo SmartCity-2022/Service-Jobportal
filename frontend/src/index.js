1000549e3050818bf39ff22224dacad9d9d616b0
cv7l1 <0xcv7l1@protonmail.com>
2022-06-06 13:53:32 +0200

    add error handler

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,29 +1,34 @@
-import './index.css';
+import './App.css';
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import App from './App';
 import Companypage from './routes/Companypage';
+import Errorpage from './routes/Errorpage';
 import Job from './routes/JobDetailpage'
 import Jobpage from './routes/Jobpage'
 import Jobsearchpage from './routes/Jobsearchpage';
 import Navbar from './components/NavigationBar';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from '@emotion/react';
+import theme from './theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Navbar/>
-
-    <BrowserRouter>
-      <Routes>
-        <Route path = "/" element = {<App/>}/>
-        <Route path = "/stellen" element = {<Jobpage/>}/>
-        <Route path = "/stellen/:id" element = {<Job/>}/>
-        <Route path = "/stellensuche" element = {<Jobsearchpage/>}/>
-        <Route path = "/meine-firmen" element = {<Companypage/>}/>
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          <Route path = "/" element = {<App/>}/>
+          <Route path = "/stellen" element = {<Jobpage/>}/>
+          <Route path = "/stellen/:id" element = {<Job/>}/>
+          <Route path = "/stellensuche" element = {<Jobsearchpage/>}/>
+          <Route path = "/meine-firmen" element = {<Companypage/>}/>
+          <Route path = "*" element = {<Errorpage status={404}/>}/>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
 );
--- frontend/src/routes/Companypage.js
+++ frontend/src/routes/Companypage.js
@@ -3,17 +3,19 @@ import "../App.css"
 import { useEffect, useState } from "react"
 
 import CompanyList from "../components/company/CompanyList"
+import Errorpage from "./Errorpage"
 import axios from "axios"
 
 const Companypage = () => {
 
+  var auth = false
   const [userCompanies,  setCompanies] = useState([])
   
   const getCompanies = async() => {
     const url = process.env.REACT_APP_API_URL + "/citizen/companies"  
     await axios.get(url, {withCredentials: true}, []).then(res => {
       setCompanies(res.data)
-      console.log("success: " + userCompanies)
+      auth = true
     })
     .catch(err => {
       console.log(err)
@@ -22,11 +24,12 @@ const Companypage = () => {
   // eslint-disable-next-line
   useEffect(() => {getCompanies()}, [])
 
-  return (
-    <>
-      <CompanyList companies={userCompanies}/>
-    </>
-  )
+  if(auth) {
+    return (<CompanyList companies={userCompanies}/>)
+  }
+  else {
+    return (<Errorpage status={401}/>)
+  }
 }
 
 export default Companypage

200047399f95680d96f774a462561281b8315ba6
cv7l1 <0xcv7l1@protonmail.com>
2022-06-06 09:23:03 +0200

    use system fonts

--- frontend/src/theme.js
+++ frontend/src/theme.js
@@ -5,6 +5,20 @@ const theme = createTheme({
     primary: {
       main: '#3f51b5',
     }
-  }
+  },
+  typography: {
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+  },
 });
 export default theme;

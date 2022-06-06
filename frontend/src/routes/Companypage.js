a9a581a83b2bc72c132a143a36d13e91020b63da
cv7l1 <0xcv7l1@protonmail.com>
2022-06-06 15:26:10 +0200

    fix conditional rendering

--- frontend/src/routes/Companypage.js
+++ frontend/src/routes/Companypage.js
@@ -6,19 +6,18 @@ import CompanyList from "../components/company/CompanyList"
 import Errorpage from "./Errorpage"
 import axios from "axios"
 
+var auth
 const Companypage = () => {
-
-  var auth = false
   const [userCompanies,  setCompanies] = useState([])
-  
+
   const getCompanies = async() => {
-    const url = process.env.REACT_APP_API_URL + "/citizen/companies"  
+    const url = process.env.REACT_APP_API_URL + "/citizen/companies"
     await axios.get(url, {withCredentials: true}, []).then(res => {
       setCompanies(res.data)
       auth = true
     })
-    .catch(err => {
-      console.log(err)
+    .catch(() => {
+      auth = false
     })
   }
   // eslint-disable-next-line

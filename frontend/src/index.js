4f2f1e990074cf12ce0fd95ee301391dc86a3c56
cv7l1 <0xcv7l1@protonmail.com>
2022-06-06 20:53:53 +0200

    random theme :)

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,21 +3,26 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import App from './App';
-import Companypage from './routes/Companypage';
+import Companydetailpage from "./routes/company/Companydetailpage";
+import Companypage from './routes/company/Companypage';
 import Errorpage from './routes/Errorpage';
-import Job from './routes/JobDetailpage'
-import Jobpage from './routes/Jobpage'
-import Jobsearchpage from './routes/Jobsearchpage';
+import Job from './routes/job/JobDetailpage'
+import Jobaddpage from "./routes/job/JobAddpage"
+import Jobpage from './routes/job/Jobpage'
+import Jobsearchpage from './routes/job/Jobsearchpage';
 import Navbar from './components/NavigationBar';
+import ProtectedRoute from './components/ProtectedRoute';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from '@emotion/react';
 import theme from './theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const themes = [theme.theme, theme.theme2, theme.theme3, theme.theme4]
+
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={themes[Math.floor(Math.random() * 4)]}>
       <BrowserRouter>
         <Navbar/>
         <Routes>
@@ -26,6 +31,9 @@ root.render(
           <Route path = "/stellen/:id" element = {<Job/>}/>
           <Route path = "/stellensuche" element = {<Jobsearchpage/>}/>
           <Route path = "/meine-firmen" element = {<Companypage/>}/>
+          <Route path = "/firma/:id" element = {<Companydetailpage/>}/>
+          <Route path = "/add" element = {<ProtectedRoute element={<Jobaddpage/>}/>}/>
+          
           <Route path = "*" element = {<Errorpage status={404}/>}/>
         </Routes>
       </BrowserRouter>
--- frontend/src/theme.js
+++ frontend/src/theme.js
@@ -1,13 +1,51 @@
 import { createTheme } from '@mui/material/styles';
 
-const theme = createTheme({
+const theme = createTheme(
+  {
   palette: {
     primary: {
-      main: '#3f51b5',
+      main: "#006666",
     }
   },
   typography: {
     fontFamily: "Open Sans"
   },
-});
-export default theme;
+})
+
+const theme2 = createTheme(
+  {
+  palette: {
+    primary: {
+      main: "#666699",
+    }
+  },
+  typography: {
+    fontFamily: "Open Sans"
+  },
+})
+
+const theme3 = createTheme(
+  {
+  palette: {
+    primary: {
+      main: "#336699",
+    }
+  },
+  typography: {
+    fontFamily: "Open Sans"
+  },
+})
+
+const theme4 = createTheme(
+  {
+  palette: {
+    primary: {
+      main: "#660033",
+    }
+  },
+  typography: {
+    fontFamily: "Open Sans"
+  },
+})
+
+export default {theme, theme2, theme3, theme4}

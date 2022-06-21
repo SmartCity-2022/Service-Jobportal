56f3b4857ac33afa1cc7a25254776e9d9fe7c74d
cv7l1 <0xcv7l1@protonmail.com>
2022-06-22 00:11:06 +0200

    add mappings for dashboard and application page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,13 +3,16 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import App from './App';
+import Applicationmanagement from './routes/application/CompanyApplicationpage';
 import Companydetailpage from "./routes/company/Companydetailpage";
 import Companypage from './routes/company/Companypage';
+import Dashboardpage from './routes/company/Dashboardpage';
 import Errorpage from './routes/Errorpage';
 import Job from './routes/job/JobDetailpage'
 import Jobaddpage from "./routes/job/JobAddpage"
 import Jobpage from './routes/job/Jobpage'
 import Jobsearchpage from './routes/job/Jobsearchpage';
+import MyApplications from './routes/application/Myapplicationspage';
 import Navbar from './components/NavigationBar';
 import ProtectedRoute from './components/ProtectedRoute';
 import React from 'react';
@@ -17,7 +20,6 @@ import ReactDOM from 'react-dom/client';
 import Submitapplicationpage from './routes/application/Submitapplicationpage';
 import { ThemeProvider } from '@emotion/react';
 import theme from './theme';
-import MyApplications from './routes/application/Myapplicationspage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const themes = [theme.theme, theme.theme2, theme.theme3, theme.theme4]
@@ -35,8 +37,10 @@ root.render(
           <Route path = "/meine-firmen" element = {<ProtectedRoute element={<Companypage/>}/>}/>
           <Route path = "/meine-bewerbungen" element = {<ProtectedRoute element={<MyApplications/>}/>}/>
           <Route path = "/firma/:id" element = {<Companydetailpage/>}/>
+          <Route path = "/firma/:id/dashboard" element = {<ProtectedRoute element={<Dashboardpage/>}/>}/>
           <Route path = "/stellenausschreibung" element = {<ProtectedRoute element={<Jobaddpage/>}/>}/>
           <Route path = "/stellen/:id/bewerbung" element = {<ProtectedRoute element= {<Submitapplicationpage/>}/>}/>
+          <Route path = "/firma/:id/dashboard/bewerbungen" element = {<ProtectedRoute element={<Applicationmanagement/>}/>}/>
 
           <Route path = "*" element = {<Errorpage status={404}/>}/>
         </Routes>

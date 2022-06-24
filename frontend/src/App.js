a94bf334ff281bdbb43cee5e0ce76de37c62e43a
cv7l1 <0xcv7l1@protonmail.com>
2022-06-24 19:07:33 +0200

    update welcome text

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,23 @@
-import './App.css';
+import './App.css'
 
-import { Typography } from '@mui/material';
+import { Typography } from '@mui/material'
 
 function App() {
 
   return (
     <div className="content">
-      <Typography variant="h4" fontWeight={600} textAlign="center" margin="2%">
+      <Typography variant="h4" fontWeight={600} textAlign="center" margin="3%">
         Microservice Jobportal
       </Typography>
-      <Typography color="text.secondary" marginLeft={"20%"} marginRight={"20%"} textAlign="center">
-        Der Microservice Jobportal befasst sich mit der Ausschreibung und Suche von Praktikumsplätzen,
-        Ausbildungsplätzen, Minijobs sowie Teilzeit und Vollzeitstellen für die Bürger von SmartCity.
+      <Typography color="text.secondary" margin={"0% 25% 0% 25%"} textAlign="center">
+      Der Microservice Jobportal befasst sich mit der Ausschreibung und Suche von Praktikumsplätzen,
+      Ausbildungsstellen, Mini- Teilzeit und Vollzeitjobs für die Bürger von SmartCity.
+      <br/>
+      Registrierte Bürger im Bürgerbüro können entweder eine Firma registrieren um Stellen auszuschreiben 
+      oder nach bereits ausgeschriebenen Stellen suchen und sich dort bewerben.
       </Typography>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
